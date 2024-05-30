4a3b5a4e64302ab16a0565089b12f9c1f28b4b1c
AbdulMoizdacoder <112655215+AbdulMoizdacoder@users.noreply.github.com>
2024-05-30 11:25:14 -0600

    working on making deck

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 #main python loop
-heart = "♥"
+
+# Variables
+hearts = "♥"
 clubs = "♣"
-diamond = "♦"
+diamonds = "♦"
 spades = "♠"
-suits = [heart, clubs,diamond, spades]
+suits = [hearts, clubs,diamonds, spades]
+ranks = ["2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE"]
+card_deck = []
+face_cards = [
+    {"KING","10"},
+    {"QUEEN","10"},
+    {"JACK","10"},
+    {"ACE","11"}
+]
+print(face_cards[3])
+class Card:
+    def __init__(self,value,suit):
+        self.value = value
+        self.suit = suit
     
-    
+    def print_card(self):
+        print(f"you drew a {self.value} of {self.suit}")
+
+
+
