d7d4000f09af2ec307d2fe8d65dda6e9759b74b3
AbdulMoizdacoder <112655215+AbdulMoizdacoder@users.noreply.github.com>
2024-05-31 11:25:05 -0600

    work on player class
    deal cards create player hand

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ diamonds = "♦"
 spades = "♠"
 suits = [hearts, clubs,diamonds, spades]
 ranks = ["2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE"]
-card_deck = []
+
 face_cards = [
+    {"ACE","11"},
     {"KING","10"},
     {"QUEEN","10"},
-    {"JACK","10"},
-    {"ACE","11"}
+    {"JACK","10"} 
 ]
 
 class Card:
@@ -23,22 +23,31 @@ class Card:
     def print_card(self):
         print(f"you drew a {self.value} of {self.suit}")
 
-class Deck:
-    def __init__(self):
-        self.cards = []
-        self.create()
+class Deck: 
+    def __init__(self): 
+        self.card_deck = []     
+        for i in range(3):
+            self.build_deck()
+            i+=1
 
-    def create(self):
+    def build_deck(self):
         for s in suits:
             for r in ranks:
-                self.cards.append(Card(r,s))
-    
-    def show(self):
-        for c in self.cards:
-            c.print_card()
+                self.card_deck.append(Card(r,s))
 
+    def show_card(self):
+        self.card_deck.print_card()
+    
+class Player:
+    def __init__(self):
+        self.hand = []
+        self.points = 0
+    
+    def deal_card(self):
+        pass
+
+            
+                
 
 deck = Deck()
-deck.show()
-
-
+deck.show_card
