20b2b0f0a8a90f2e2df36291c30982f2c4333b86
AbdulMoizdacoder <112655215+AbdulMoizdacoder@users.noreply.github.com>
2024-06-13 11:17:03 -0600

    truly cooked

--- main.py
+++ main.py
@@ -21,15 +21,18 @@ class Card:
 class Deck: 
     def __init__(self): 
         self.card_deck = []     
-        for i in range(3):
-            self.build_deck()
-            i+=1
+        self.full_deck()
         self.shuffle_deck()
 
     def build_deck(self):
         for s in suits:
             for r in ranks:
                 self.card_deck.append(Card(r,s))
+    def full_deck(self):
+          for i in range(6):
+            self.build_deck()
+            i+=1
+    
 
     def get_card(self):
         card = self.card_deck.pop()
@@ -47,8 +50,12 @@ class Player():
         for i in range(times):
             user_card = deck.get_card()
             self.hand.append(user_card)
-            self.hand[-1].print_card()
             i +=1
+
+    def show_hand(self):
+        for j in range(len(self.hand)):
+            self.hand[j].print_card()
+            j+=1
 
 class Game():
     def __init__(self):
@@ -65,12 +72,27 @@ class Game():
     elif user_start_input == "y":   
         loop=True
     while loop:
-            print(f"you drew")
+        if deck.card_deck != []:
+            print(f"\nyou drew")
             player.draw(2,deck)
-            #draw card for computer
+            player.show_hand()
             print("\ndealer draws one card")
             computer.draw(1,deck)
-            
+            computer.show_hand()
+            user_input1 = input("\nWould like hit or stand? hit/stand: ")
+            while 
+            if user_input1 == "hit":
+                player.draw(1,deck)
+                player.show_hand()
+
+            elif user_input1 == "stand":
+                #draw card for computer
+                print("\ndealer draws one card")
+                computer.draw(1,deck)
+                computer.show_hand()
+        else:
+            pass
+
 
             
             
